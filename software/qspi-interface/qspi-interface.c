e1fda2b54fcca60689a6c269000dbb20e9065695
Haotian Wang <haotian.wang@sifive.com>
2019-05-31 17:07:54 -0700

    switch to RMES command to test the SPI interface. SPI interface not working as intended at this point

--- software/qspi-interface/qspi-interface.c
+++ software/qspi-interface/qspi-interface.c
@@ -2,7 +2,6 @@
 #include <metal/spi.h>
 
 int main() {
-	puts("test");
 	/* Instruction Hex code according to Cypress datasheet */
 	
 	/* Read 4-byte address */
@@ -10,6 +9,9 @@ int main() {
 	
 	/* Read device ID */
 	const char RDID = 0x9F;
+
+	/* Read device electronic signauture */
+	const char REMS = 0x90;
 
 	/* Iterate over a few device numbers to select the first available SPI device */
 	int number_of_tries = 0;
@@ -24,12 +26,17 @@ int main() {
 		return 1;
 	}
 
+	/* Report the device ID of the SPI device */
+	char buffer[50];
+	sprintf(buffer, "The device id of the spi device is %d", number_of_tries);
+	puts(buffer);
+
 	/* Initialize SPI device to 100,000 baud rate */
 	metal_spi_init(spi, 100000);
 
 	/* CPOL = 0, CPHA = 0, quad spi, MSb-first, CS active low */
 	struct metal_spi_config config = {
-		.protocol = METAL_SPI_QUAD,
+		.protocol = METAL_SPI_SINGLE,
 		.polarity = 0,
 		.phase = 0,
 		.little_endian = 0,
@@ -37,10 +44,10 @@ int main() {
 		.csid = 0,
 	};
 
-	/* Instruction for reading a 4-byte address */
-	char instruction[1] = {RDID};		
-	char receive_buffer[1] = {0};
-	int transfer_success = metal_spi_transfer(spi, &config, 1, instruction, receive_buffer);
+	/* Instruction for reading identification information of the spansion flash memory */
+	char instruction[4] = {RDID, 0, 0, 0};		
+	char receive_buffer[4] = {0};
+	int transfer_success = metal_spi_transfer(spi, &config, 4, instruction, receive_buffer);
 	if (transfer_success != 0) {
 		puts("spi transfer failed.");
 		return 1;
